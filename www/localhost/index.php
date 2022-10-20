8feed045dfd95af39ec1b27feb7e6c0e5c08e0ec
曹高煜 <1765841705@qq.com>
2022-10-20 10:09:28 +0800

    修改：index.php

--- www/localhost/index.php
+++ www/localhost/index.php
@@ -18,7 +18,7 @@ echo '<li>PHP版本：', PHP_VERSION, '</li>';
 echo '<li>Nginx版本：', $_SERVER['SERVER_SOFTWARE'], '</li>';
 echo '<li>MySQL服务器版本：', getMysqlVersion(), '</li>';
 echo '<li>Redis服务器版本：', getRedisVersion(), '</li>';
-//echo '<li>MongoDB服务器版本：', getMongoVersion(), '</li>';
+echo '<li>MongoDB服务器版本：', getMongoVersion(), '</li>';
 echo '</ul>';
 
 echo '<h2>已安装扩展</h2>';
@@ -77,12 +77,12 @@ function getMongoVersion()
 {
     if (extension_loaded('mongodb')) {
         try {
-            $manager = new MongoDB\Driver\Manager('mongodb://root:123456@mongodb:27017');
+            $manager = new MongoDB\Driver\Manager('mongodb://root:root@mongo60:27017');
             $command = new MongoDB\Driver\Command(array('serverStatus'=>true));
 
-            $cursor = $manager->executeCommand('admin', $command);
+            $cursor = $manager->executeCommand('admin', $command)->toArray();
 
-            return $cursor->toArray()[0]->version;
+            return $cursor[0]->version;
         } catch (Exception $e) {
             return $e->getMessage();
         }
