9a7ffa467855d6e1a0f5d63783fa04cb31a27441
xiaoyu <1765841705@qq.com>
2023-08-23 18:06:37 +0800

    update: localhost.conf

--- www/localhost/index.php
+++ www/localhost/index.php
@@ -6,7 +6,7 @@ echo '<h1 style="text-align: center;">欢迎使用DNMP！</h1>';
 echo '<h2>当前时间：<span id="current-time">'  . date("Y-m-d H:i:s") .  '</span></h2>';
 echo '<h2>版本选择：</h2>';
 echo '<div style="display:flex;justify-content: space-evenly;">';
-echo '<h3><a href="/">PHP7.2</a></h3>';
+echo '<h3><a href="/72/">PHP7.2</a></h3>';
 echo '<h3><a href="/73/">PHP7.3</a></h3>';
 echo '<h3><a href="/74/">PHP7.4</a></h3>';
 echo '<h3><a href="/80/">PHP8.0</a></h3>';
