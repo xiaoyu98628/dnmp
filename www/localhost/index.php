73585e2c1d8c5de9edbbebdb03a5b9af0041823f
xiaoyu <1765841705@qq.com>
2023-08-24 09:28:43 +0800

    update: index.html index.php

--- www/localhost/index.php
+++ www/localhost/index.php
@@ -4,18 +4,19 @@ ini_set('display_errors', 1);
 
 echo '<h1 style="text-align: center;">欢迎使用DNMP！</h1>';
 echo '<h2>当前时间：<span id="current-time">'  . date("Y-m-d H:i:s") .  '</span></h2>';
-echo '<h2>版本选择：</h2>';
-echo '<div style="display:flex;justify-content:space-between;">';
-echo '<h3><a href="/">Home</a></h3>';
-echo '<h3><a href="/72/">PHP7.2</a></h3>';
-echo '<h3><a href="/73/">PHP7.3</a></h3>';
-echo '<h3><a href="/74/">PHP7.4</a></h3>';
-echo '<h3><a href="/80/">PHP8.0</a></h3>';
-echo '<h3><a href="/81/">PHP8.1</a></h3>';
-echo '<h3><a href="/82/">PHP8.2</a></h3>';
-echo '</div>';
-echo '<h2>版本信息：</h2>';
 
+echo '<div style="display: flex;justify-content: space-between;font-size: 24px;font-weight: bolder">';
+echo '<div>版本选择：</div>';
+echo '<div><a href="/">Home</a></div>';
+echo '<div><a href="/72/">PHP7.2</a></div>';
+echo '<div><a href="/73/">PHP7.3</a></div>';
+echo '<div><a href="/74/">PHP7.4</a></div>';
+echo '<div><a href="/80/">PHP8.0</a></div>';
+echo '<div><a href="/81/">PHP8.1</a></div>';
+echo '<div><a href="/82/">PHP8.2</a></div>';
+echo '</div>';
+
+echo '<h2>版本信息：</h2>';
 echo '<ul>';
 echo '<li>PHP版本：', PHP_VERSION, '</li>';
 echo '<li>服务器软件信息：', $_SERVER['SERVER_SOFTWARE'], '</li>';
